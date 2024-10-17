e981bb83dafbcae25de1c31b1372faed7bdb8a67
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2024-10-17 22:26:33 +0330

    :sparkles: feat(models): Add UserAnnouncementProfile model to link users with announcement audiences
    - Introduced the UserAnnouncementProfile model to associate users with multiple audiences for targeted announcements.
    - Added a OneToOneField to link each user with a unique announcement profile.
    - Added a ManyToManyField to manage relationships between users and audiences.
    - Cached the get_username utility to return the user's username in the string representation of the profile.
    - Inherited from TimeStampedModel to include created_at and updated_at timestamps.
    - Defined __str__ method to return the username of the associated user for better readability.
    
    Closes #6

---
+++ django_announcement/models/__init__.py
@@ -0,0 +1,4 @@
+from .announcement import Announcement
+from .announcement_category import AnnouncementCategory
+from .audience import Audience
+from .user_event_profile import UserAnnouncementProfile
