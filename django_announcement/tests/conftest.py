320e56268550cba375796e44ef3cee21cf230113
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2024-10-23 22:16:58 +0330

    :rotating_light: tests: Add basic tests configs and fixtures

---
+++ django_announcement/tests/conftest.py
@@ -0,0 +1,14 @@
+from django_announcement.tests.fixtures import (
+    admin_user,
+    announce_profile_admin,
+    announcement,
+    announcement_admin,
+    announcement_category,
+    api_client,
+    audience,
+    mock_request,
+    setup_data,
+    user,
+    user_announcement_profile,
+)
+from django_announcement.tests.setup import configure_django_settings
